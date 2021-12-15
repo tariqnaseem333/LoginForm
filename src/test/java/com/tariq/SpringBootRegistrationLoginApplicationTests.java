ecc0b20e70983d2d674ac50a2dba86ed77236808
tariqnaseem333 <tariqnaseem333@gmail.com>
2021-12-15 21:10:05 +0530

    Initial Commit

---
+++ src/test/java/com/tariq/SpringBootRegistrationLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tariq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
